0e23c24d56cd7fcc7f4c054dbcdb2c470c6aed07
Priyansh Dubey <priyansh.dubey@walmart.com>
2025-11-10 18:32:31 +0530

    Added MeetingRooms solutions.

---
+++ easy/meetingRooms/Interval.java
@@ -0,0 +1,12 @@
+package easy.meetingRooms;
+
+// Definition of Interval:
+public class Interval {
+  public int start, end;
+  public Interval(int start, int end) {
+      this.start = start;
+      this.end = end;
+  }
+}
+
+
---
+++ easy/meetingRooms/sol.java
@@ -0,0 +1,28 @@
+package easy.meetingRooms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+
+        intervals.sort((a, b) -> a.start - b.start);
+
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (intervals.get(i).end > intervals.get(i + 1).start) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main() {
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 3));
+        intervals.add(new Interval(4, 5));
+        intervals.add(new Interval(7, 9));
+        intervals.add(new Interval(9, 10));
+
+        System.out.println("Possiible = " + new Solution().canAttendMeetings(intervals));
+    }
+}
